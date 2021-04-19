aaf43b51bb39a2b47ce4cf7d70523bcbe751bbc1
root <root@Omega-2A15.lan>
2021-04-19 23:36:12 +0200

    Improved code for scanning files/folders

--- scan.php
+++ scan.php
@@ -9,56 +9,36 @@ $response = scan($dir);
 
 // This function scans the files folder recursively, and builds a large array
 
-function scan($dir){
-
+function scan($dir) {
 	$files = array();
 
 	// Is there actually such a folder/file?
-
-	if(file_exists($dir)){
-	
-		foreach(scandir($dir) as $f) {
-		
-			if(!$f || $f[0] == '.') {
+	if (file_exists($dir)) {
+		foreach (scandir($dir) as $f) {
+			if (!$f || $f[0] == '.') {
 				continue; // Ignore hidden files
 			}
-
-			if(is_dir($dir . '/' . $f)) {
-
-				// The path is a folder
-
-				$files[] = array(
-					"name" => $f,
-					"type" => "folder",
-					"path" => $dir . '/' . $f,
-					"items" => scan($dir . '/' . $f) // Recursively get the contents of the folder
-				);
-			}
 			
-			else {
-
-				// It is a file
-
-				$files[] = array(
-					"name" => $f,
-					"type" => "file",
-					"path" => $dir . '/' . $f,
-					"size" => filesize($dir . '/' . $f) // Gets the size of this file
-				);
-			}
+			$path = "$dir/$f";
+			$is_dir = is_dir($path);
+			$key = $is_dir ? 'items' : 'size';
+			
+			$files[] = array(
+				'name' => $f,
+				'type' => $is_dir ? 'folder' : 'file',
+				'path' => $path,
+				$key => $is_dir ? scan($path) : filesize($path)
+			);
 		}
-	
 	}
 
 	return $files;
 }
 
 
-
 // Output the directory listing as JSON
 
 header('Content-type: application/json');
-
 echo json_encode(array(
 	"name" => "files",
 	"type" => "folder",
